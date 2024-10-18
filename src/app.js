f86f98eecfecc548f7d9600594fb660304712b21
Judit <judit.arman95@gmail.com>
2024-10-18 19:29:23 +0000

    arreglado el error

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "us", "they"];
+  let adj = ["great", "big", "small", "kind"];
+  let noun = ["jogger", "racoon", "cat", "house", "Sabrina"];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        console.log(pronoun[a] + adj[b] + noun[c] + ".com");
+      }
+    }
+  }
 };
